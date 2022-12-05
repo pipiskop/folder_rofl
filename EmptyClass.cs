69ffd4afd1d47407f82a64bea840e1b6f13be84b
pipiskop <isip_s.a.krapovnicky@mpt.ru>
2022-12-05 15:33:00 +0300

    проводник для винды на маке погнали

--- EmptyClass.cs
+++ EmptyClass.cs
@@ -1,11 +1,50 @@
 ﻿using System;
-namespace folder
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace francyzskayaPracticheskaya
 {
-    public class EmptyClass
+    public static class GetFiles
     {
-        public EmptyClass()
+        public static List<Failik> Get(string path)
         {
+            List<Failik> failikList = new List<Failik>();
+            foreach (string file in Directory.GetDirectories(path))
+                failikList.Add(new Failik(file.Split("\\")[^1] + "\\", file));
+            foreach (string file in Directory.GetFiles(path))
+                failikList.Add(new Failik(file.Split("\\")[^1], file));
+            return failikList;
         }
     }
-}
+    public class Kursor
+    {
+        public Kursor(int max = 1, int min = 1, int pos = 1)
+        {
+            this.max = max;
+            this.min = min;
+            this.pos = pos;
+        }
+        public int max, min, pos;
+    }
+    public static class ShowInformation
+    {
+        public static void ShowDirs(List<Failik> papkaList)
+        {
+            foreach (Failik elem in papkaList)
+                Console.WriteLine("   " + elem.name);
+        }
+    }
+    public class Failik
+    {
+        public Failik(string name, string path)
+        {
 
+            this.path = path;
+            this.name = name;
+        }
+        public string name;
+        public string path;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,33 +1,80 @@
-﻿using System.IO;
-using System.Runtime.CompilerServices;
-using System.Xml.Serialization;
-using Newtonsoft.Json;
-namespace converter6practa
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+
+namespace francyzskayaPracticheskaya
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Путь до файла");
-            string putDoFaila = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("Нажмите F1 чтобы сохранить файл");
-            Console.WriteLine(Preobrazovanie.ConvertToText(putDoFaila));
+            string nashaPapka = "\\";
+            Kursor kursor = new Kursor();
+            List<Failik> papkaList = new List<Failik>();
             while (true)
             {
-                if (Console.ReadKey().Key == ConsoleKey.F1)
+                Console.SetCursorPosition(0, 0);
+                kursor.max = 0;
+                Console.WriteLine("Проводник.ехе. Текущий путь - " + nashaPapka);
+                if (nashaPapka == "\\")
                 {
-                    break;
-                };
-            }
-            Console.Clear();
-            Console.WriteLine("Путь до файла чтобы сохранить");
-            string putDlyaSohranenia = Console.ReadLine();
-            Preobrazovanie.SaveFile(putDoFaila, putDlyaSohranenia);
-            Console.Clear();
-            Console.WriteLine("Сохранено!");
+                    papkaList = new List<Failik>();
+                    DriveInfo[] allDrives = DriveInfo.GetDrives();
+                    foreach (DriveInfo drive in allDrives)
+                    {
+                        double totalSpace = drive.TotalSize;
+                        double availableSpace = drive.AvailableFreeSpace;
+                        Failik elem = new Failik($"Диск {drive.Name} | Размер {totalSpace} гб | Доступно {availableSpace} гб", drive.Name);
+                        papkaList.Add(elem);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        papkaList = GetFiles.Get(nashaPapka);
+                    }
+                    catch
+                    {
 
+                    }
+
+                }
+                ShowInformation.ShowDirs(papkaList);
+                kursor.max = papkaList.Count;
+                Console.SetCursorPosition(0, kursor.pos);
+                Console.Write(">>");
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Enter:
+                        if (Directory.Exists(papkaList[kursor.pos - kursor.min].path))
+                        {
+                            nashaPapka = papkaList[kursor.pos - kursor.min].path;
+                        }
+                        else
+                        {
+                            Process.Start(papkaList[kursor.pos - kursor.min].path);
+                        }
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (kursor.pos == kursor.max)
+                            kursor.pos = kursor.min;
+                        else
+                            kursor.pos++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (kursor.pos == kursor.min)
+                            kursor.pos = kursor.max;
+                        else
+                            kursor.pos--;
+                        break;
+                    case ConsoleKey.Escape:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
         }
     }
 }
